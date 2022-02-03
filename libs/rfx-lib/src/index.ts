5ccb6e12306f764253e9624f1f616b59e557714a
p17875 <raphael.felber@css.ch>
2022-02-03 12:22:50 +0100

    SEB-29478: Added: FormDefinitionAny to index too

--- libs/rfx-lib/src/index.ts
+++ libs/rfx-lib/src/index.ts
@@ -3,6 +3,7 @@ export { FormRegistry } from './lib/forms/form-registry.service';
 export { ObserveFormPipe, ObserveFormType, ObserveFormPipeOptions } from './lib/forms/observe-form.pipe';
 export { TypedFormControl, TypedFormGroup, TypedFormArray } from './lib/forms/typed-form-control';
 export {
+  FormDefinitionAny,
   FormDefinitionField,
   FormDefinitionCustomField,
   FormDefinitionGroup,
